d45101720811e7f556aeaa326a356da74e0b9209
Balamurugan R <balamuruganrdckap@gmail.com>
2023-09-21 09:53:25 +0530

    React components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderNavbar from './HeaderNavbar';
+import Hero from './Hero';
+import About from './About';
+import Services from './Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderNavbar />
+      <Hero />
+      <main id="main">
+        <About />
+        <Services />
+      </main>
     </div>
   );
 }
